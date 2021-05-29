d9781cfc8a0774f2fbd6f4694e89440cde50f862
shubham <pwrs.0007@gmail.com>
2021-05-29 23:14:11 +0530

    changed session store

--- app.js
+++ app.js
@@ -61,9 +61,21 @@ const sessionConfig = {
 
 app.use(
   cors({
+    credentials: true,
     origin: "https://foodhub-c3fd3.web.app",
   })
 );
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
